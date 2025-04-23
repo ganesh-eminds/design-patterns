fae96d1b0281425f15752eb74f70e57bfcaa5887
Ganesh Yelagandula <ganesh.y@eminds.com>
2025-04-23 15:44:30 +0530

    removing unnecessary things

---
+++ catalog-service/src/main/java/com/ecommerce/os/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.os.repository;
+
+import com.ecommerce.os.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order,Integer> {
+
+    List<Order> findByCategory(String category);
+
+
+}
